342f2e6bd51588f8c0c96d6f2379592cd87b804c
nickolay-kondratyev <nickolay.kondratyev@gmail.com>
2022-01-18 14:43:32 +0700

    SAVE

--- packages/engine-test-utils/jest.config.js
+++ packages/engine-test-utils/jest.config.js
@@ -1,6 +1,20 @@
 const jestConfig = require("../../jest.config");
 
+const TEST_FILES_REGEX = '.*engine-test-utils.*(/test/.*|(\\.|/)(test|spec))\\.ts$';
+
 module.exports = {
   ...jestConfig,
   testEnvironment: "jest-environment-jsdom-sixteen",
+  collectCoverage: true,
+  /** According to this post https://stackoverflow.com/a/59717197/7858768
+   *  Jest will only pick up code coverage from files that are within the root
+   *  dir hierarchy.*/
+  rootDir: "./../",
+  testRegex: TEST_FILES_REGEX,
+  collectCoverageFrom: [
+   // "**/src/**/*",
+
+    // Exclude snapshots from code coverage tests:
+    "!**/__snapshots__/**",
+  ],
 };
